1ca4a73e6b04b1d5db54935783b827345511c50b
Milena May <MilenaMMay@googlemail.com>
2017-09-22 17:32:12 +0200

    Let angular call rails

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
-  selector: 'hello-angular',
-  template: `<h1>Hello {{name}}</h1>`
+    selector: 'hello-angular',
+    template: `<h1>Hello {{name}}</h1>`
 })
+
+@Injectable()
 export class AppComponent {
-  name = 'Angular!';
+    name = 'Angular!';
+
+    constructor(private http: Http) {
+        this.http
+            .get('/api/title')
+            .subscribe((data) => {
+                this.name = data.json().title
+            })
+    }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get '/', to: 'root#index'
 
+  get '/api/title', to: 'title#index'
 end
